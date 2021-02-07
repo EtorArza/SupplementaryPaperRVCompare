857ea64fe7ca544d825393924ab02bd4c3f1ec0c
EtorArza <etorarza@gmail.com>
2021-02-07 14:15:34 +0100

    initial commit

---
+++ get_sample.py
@@ -0,0 +1,138 @@
+"""
+Title: Simple MNIST convnet
+Author: [fchollet](https://twitter.com/fchollet)
+Date created: 2015/06/19
+Last modified: 2020/04/21
+Description: A simple convnet that achieves ~99% test accuracy on MNIST.
+"""
+from tqdm import tqdm as tqdm
+import os
+import random
+import numpy as np
+import tensorflow as tf
+from keras import backend as K
+import numpy as np
+from tensorflow import keras
+from tensorflow.keras import layers
+
+RESULT_PATH = 'data/scores.csv'
+THREADS = 6
+
+
+def touch(fname):
+    if os.path.exists(fname):
+        os.utime(fname, None)
+    else:
+        open(fname, 'a').close()
+
+touch(RESULT_PATH)
+
+
+while True:
+    with open(RESULT_PATH, 'r') as f:
+        seed = len(f.readlines()) + 2
+    print("-----------")
+    print("seed = ",seed)
+
+    """
+    ## Setup
+    """
+
+    # Seed value
+    # Apparently you may use different seed values at each stage
+    seed_value = seed
+
+    # 1. Set `PYTHONHASHSEED` environment variable at a fixed value
+    os.environ['PYTHONHASHSEED']=str(seed_value)
+
+    # 2. Set `python` built-in pseudo-random generator at a fixed value
+    random.seed(seed_value)
+
+    # 3. Set `numpy` pseudo-random generator at a fixed value
+    np.random.seed(seed_value)
+
+    # 4. Set the `tensorflow` pseudo-random generator at a fixed value
+    tf.random.set_seed(seed_value)
+    # for later versions: 
+    # tf.compat.v1.set_random_seed(seed_value)
+
+    # 5. Configure a new global `tensorflow` session
+    session_conf = tf.compat.v1.ConfigProto(intra_op_parallelism_threads=THREADS, inter_op_parallelism_threads=1)
+    sess = tf.compat.v1.Session(graph=tf.compat.v1.get_default_graph(), config=session_conf)
+    tf.compat.v1.keras.backend.set_session(sess)
+
+
+
+
+
+    # https://github.com/keras-team/keras-io/blob/master/examples/vision/mnist_convnet.py
+
+    """
+    ## Prepare the data
+    """
+
+    # Model / data parameters
+    num_classes = 10
+    input_shape = (28, 28, 1)
+
+    # the data, split between train and test sets
+    (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
+
+    # Scale images to the [0, 1] range
+    x_train = x_train.astype("float32") / 255
+    x_test = x_test.astype("float32") / 255
+    # Make sure images have shape (28, 28, 1)
+    x_train = np.expand_dims(x_train, -1)
+    x_test = np.expand_dims(x_test, -1)
+    # print("x_train shape:", x_train.shape)
+    # print(x_train.shape[0], "train samples")
+    # print(x_test.shape[0], "test samples")
+
+
+    # convert class vectors to binary class matrices
+    y_train = keras.utils.to_categorical(y_train, num_classes)
+    y_test = keras.utils.to_categorical(y_test, num_classes)
+
+    """
+    ## Build the model
+    """
+
+    model = keras.Sequential(
+        [
+            keras.Input(shape=input_shape),
+            layers.Conv2D(32, kernel_size=(3, 3), activation="relu"),
+            layers.MaxPooling2D(pool_size=(2, 2)),
+            layers.Conv2D(64, kernel_size=(3, 3), activation="relu"),
+            layers.MaxPooling2D(pool_size=(2, 2)),
+            layers.Flatten(),
+            layers.Dropout(0.5),
+            layers.Dense(num_classes, activation="softmax"),
+        ]
+    )
+
+    # model.summary()
+
+    """
+    ## Train the model
+    """
+
+    batch_size = 128
+    epochs = 1
+
+    model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
+
+
+
+    model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, validation_split=0.1, verbose = 0)
+
+    """
+    ## Evaluate the trained model
+    """
+
+    score = model.evaluate(x_test, y_test, verbose=0)
+    print(model.metrics_names)
+    print(score)
+    # print("Test loss:", score[0])
+    # print("Test accuracy:", score[1])
+    with open(RESULT_PATH, "a") as f:
+        print(1.0 - score[1], file = f)
---
+++ plot_distribution_X1.py
@@ -0,0 +1,38 @@
+import pandas as pd
+from matplotlib import pyplot as plt
+import numpy as np
+from scipy import stats 
+
+
+# https://en.wikipedia.org/wiki/Freedman%E2%80%93Diaconis_rule
+def n_bins_freedman_diaconis(array_x):
+    IQR = stats.iqr(array_x, interpolation = 'midpoint') 
+    res = 2.0 / float(len(array_x))**(1.0 / 3.0) * IQR
+    
+
+    return int( (max(array_x) - min(array_x)) / res )
+
+
+
+
+
+
+
+# plot distribution of X_1
+
+df = pd.read_csv('data/scores_old.csv', header=None)
+array = np.array((df.iloc[:,0]))
+print("The fiure wil he ", n_bins_freedman_diaconis(array), " b")
+
+fig, ax  = plt.subplots()
+
+ax.hist(array, bins=n_bins_freedman_diaconis(array), density=True)
+
+props = dict(boxstyle='round', facecolor='white', alpha=0.25)
+ax.text(0.95, 0.95, f"$n = {len(array)}$",  color='grey',  transform=ax.transAxes,  verticalalignment='top', horizontalalignment='right', bbox=props)
+plt.xlabel("Error rate")
+plt.ylabel("Probability density")
+
+plt.savefig('figures/X1_dist.pdf')
+
+
