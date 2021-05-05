04762c408ace9353bc9c345ae17dfdd661677405
Etor Arza <etorarza@gmail.com>
2021-05-05 12:05:33 +0200

    reduce figure size

--- plot_distribution_X1.py
+++ plot_distribution_X1.py
@@ -5,21 +5,13 @@ from scipy import stats
 from sklearn.neighbors import KernelDensity
 
 
-# https://en.wikipedia.org/wiki/Freedman%E2%80%93Diaconis_rule
-def n_bins_freedman_diaconis(array_x):
-    IQR = stats.iqr(array_x, interpolation = 'midpoint') 
-    res = 2.0 / float(len(array_x))**(1.0 / 3.0) * IQR
-    
-
-    return int( (max(array_x) - min(array_x)) / res )
-
-
-
 
 
 # plot distribution of X_A, X_B
 
 fig, ax = plt.subplots(1, 1)
+fig.set_size_inches(6, 2)
+
 
 x_min = 1e20
 x_max = -1e20
@@ -39,7 +31,6 @@ for i in range(2):
 
 
     array = np.array((df.iloc[:,0]))
-    nbins_diaconis = n_bins_freedman_diaconis(array)
     band_width = 0.0005 #(max(array) - min(array)) / nbins_diaconis
 
     print(f"Band size: {band_width}")
@@ -48,6 +39,8 @@ for i in range(2):
     X_plot = np.linspace(x_min, x_max, 10000)[:, np.newaxis]
 
 
+    array = array[~np.isnan(array)]
+ 
     kde = KernelDensity(kernel='tophat', bandwidth=band_width).fit(array[:, np.newaxis])
     Y_plot = np.exp(kde.score_samples(X_plot))
     y_max = max(Y_plot.max().max(), y_max)
@@ -65,7 +58,7 @@ for i in range(2):
 
 ax.text(0.95, 0.95, "Kernel size",  color='grey',  transform=ax.transAxes,  verticalalignment='top', horizontalalignment='right', bbox=props)
 print(np.array([band_width/2]) + x_min + 0.9 * (x_max - x_min))
-ax.plot(np.array([-band_width/2,-band_width/2,-band_width/2,band_width/2,band_width/2,band_width/2]) + x_min + 0.91 * (x_max - x_min), [y_max*0.91, y_max*0.89, y_max*0.90, y_max*0.90, y_max*0.91,y_max*0.89], color = 'grey', lw=1.5)
+ax.plot(np.array([-band_width/2,-band_width/2,-band_width/2,band_width/2,band_width/2,band_width/2]) + x_min + 0.91 * (x_max - x_min), np.array([y_max*0.81, y_max*0.79, y_max*0.80, y_max*0.80, y_max*0.81,y_max*0.79]), color = 'grey', lw=1.5)
 
 
 
