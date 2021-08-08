2a09adfdda226d17e83a76eb54550135f502d27e
EtorArza <etorarza@gmail.com>
2021-08-08 14:45:35 +0200

    test cumulative difference plot

---
+++ test_cum_diff_plot.R
@@ -0,0 +1,24 @@
+source("~/Dropbox/BCAM/06_comparing_optimization_algorithms/RVCompare/R/RVCompare.R")
+
+f1 <- c(1,2,3,4)
+f2 <- c(2,3,2,3)
+
+
+
+ranksObj <- ranksOfObserved(f1, f2)
+print(ranksObj$rank_interval_multA)
+
+density_Y_A <- helper_from_ranks_to_integrable_values(ranksObj$rank_interval_multA, j_max = 200, cumulative = FALSE)
+
+print(density_Y_A)
+
+print(helperTrapezoidRule(density_Y_A))
+
+
+print(ranksObj)
+
+
+estimated_Y_AB_bounds <- get_Y_AB_bounds_bootstrap(f1, f2, alpha = 0.1, nOfBootstrapSamples = 1e3, ignoreMinimumLengthCheck = TRUE)
+plot_Y_AB(estimated_Y_AB_bounds)
+
+
